88ae426f1e86e30cc1d6bf4aae66831be1d8f704
zeannn-spec <za03575@gmail.com>
2026-06-25 03:57:36 +0700

    Update server responses and add donation webhook

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+let lastDonation = {};
+
 app.get("/", (req, res) => {
-    res.send("OK");
+    res.send("Backend Roblox Aktif!");
+});
+
+app.post("/webhook", (req, res) => {
+    console.log("Saweria:", req.body);
+
+    lastDonation = {
+        nama: req.body.donator || "Unknown",
+        pesan: req.body.message || "",
+        nominal: req.body.amount || 0
+    };
+
+    res.sendStatus(200);
+});
+
+app.get("/donation", (req, res) => {
+    res.json(lastDonation);
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("RUNNING", PORT);
+    console.log(`RUNNING ${PORT}`);
 });
