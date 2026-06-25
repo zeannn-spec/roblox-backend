6bf7efcaf5a5e8cdc27922089c6d180c87595a47
zeannn-spec <za03575@gmail.com>
2026-06-25 11:02:30 +0700

    Change donation storage from object to array

--- server.js
+++ server.js
@@ -3,26 +3,28 @@ const app = express();
 
 app.use(express.json());
 
-let lastDonation = {};
+let donations = [];
 let donationId = 0;
 
 app.post("/webhook", (req, res) => {
     donationId++;
 
-    lastDonation = {
+    const donation = {
         id: donationId,
         donor: req.body.donator_name,
         amount: req.body.amount_raw,
         message: req.body.message || ""
     };
 
-    console.log("Donasi baru:", lastDonation);
+    donations.push(donation);
+
+    console.log("Donasi baru:", donation);
 
     res.sendStatus(200);
 });
 
 app.get("/donation", (req, res) => {
-    res.json(lastDonation);
+    res.json(donations);
 });
 
 const PORT = process.env.PORT || 3000;
